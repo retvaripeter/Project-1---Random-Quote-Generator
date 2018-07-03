d45b3d0978c25043681da764a6125554a264e0a5
Peter Retvari <pretvari@gmail.com>
2018-07-03 13:56:38 +0200

    Disabled the button
    I disabled the button until I finish with the rest of the project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+// I disabled this button until I finish with the rest of the project: document.getElementById('loadQuote').addEventListener("click", printQuote, false);
